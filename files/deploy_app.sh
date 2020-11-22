331f8f32063fa5ce9231a1cd95235a4de92ff9b7
vicsog <52973515+vicsog@users.noreply.github.com>
2020-11-22 18:49:28 +0000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,8 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. It's about to go downnn again again!!!
+  Welcome to ${PREFIX}'s app. It's about to go down!!
+  Specially dedicated to the cat lovers out there!
   <!-- END -->
 
   </div>
